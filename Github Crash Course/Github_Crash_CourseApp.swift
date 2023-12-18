04158fb37d8dcbfbf1eaa04bebec8b2d757c03d3
Umesh Kumar <umesh.k@itcomp.org>
2023-12-18 21:15:19 +0530

    Initial commit

---
+++ Github Crash Course/Github_Crash_CourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_Crash_CourseApp.swift
+//  Github Crash Course
+//
+//  Created by parth on 18/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_Crash_CourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
